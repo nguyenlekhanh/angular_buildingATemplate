05a854d694151bf6005b16bb50f145a87fbc76f7
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-08 12:04:23 -0600

    build a simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+import { HeroFormComponent } from './hero-form/hero-form.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeroFormComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
